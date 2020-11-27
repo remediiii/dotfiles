af061c5e3e69a083f1e5fa49a4d937bd0db440f3
Will Gesler <willgesler@gmail.com>
2020-11-26 22:43:06 -0500

    added .sh extensions, supercharged move-configs.sh

---
+++ move-configs.sh
@@ -0,0 +1,20 @@
+read -p "Install init.vim for neovim? [y/n] " answer
+
+while true
+do
+    case $answer in
+        [yY]* ) 
+            echo "Installing neovim config..."
+            mkdir -pv ~/.config/nvim/
+            cp init.vim ~/.config/nvim/
+            break;;
+
+        [nN]* ) 
+            echo "Not installing neovim config..."
+            break;;
+
+        * ) echo "Invalid option entered."
+    esac
+done
+
+cp .zshrc .p10k.zsh ~
