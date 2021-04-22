e7cad73a429ad84df184af2815c537fcda342671
Will Gesler <willgesler@gmail.com>
2021-04-22 16:42:21 -0400

    spring cleaning, and scripts are now fancy colors

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # dotfiles
 my dotfiles for linux
 
-requires oh-my-zsh, powerlevel10k, and vim-plug if using neovim config
+neovim is my default editor, so change your .zshrc EDITOR variable accordingly.
 
-neovim is my default editor, so change your .zshrc EDITOR variable accordingly
+## requirements
 
-update function requires redhat-lsb on Fedora
+* oh-my-zsh
+* powerlevel10k
+* vim-plug (optional, only if using neovim config)
+* (if running Fedora, for update function) redhat-lsb
+
+## installation
+
+run `./install-things.sh` if you need omz, p10k, or vimplug, then run `./move-configs` to actually move the dotfiles to where they need to be.
+both scripts give options for what you can install, so pick and choose what you want.
--- move-configs.sh
+++ move-configs.sh
@@ -1,45 +1,67 @@
-read -p "Install init.vim for neovim? [y/n] " answer
+RED='\033[1;31m'
+BG_RED='\033[1;41m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+BG_GREEN='\033[1;42m'
+BLACK='\033[1;30m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 while true
 do
+    read -p "$(echo ${BLUE}"Install init.vim for neovim? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "Installing neovim config..."
-            mkdir -pv ~/.config/nvim/
-            cp init.vim ~/.config/nvim/
+            echo "${GREEN}Installing neovim config..."
+            #mkdir -pv ~/.config/nvim/
+            #cp init.vim ~/.config/nvim/
             break;;
 
         [nN]* ) 
-            echo "Not installing neovim config..."
+            echo "${YELLOW}Not installing neovim config..."
             break;;
 
-        * ) echo "Invalid option entered."
+        * ) echo "${RED}Invalid option entered."
+
     esac
 done
-
-
-read -p "Install neofetch config? [y/n] " answer
 
 while true
 do
+    read -p "$(echo ${BLUE}"Install neofetch config? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "Installing neofetch config..."
-            mkdir -pv ~/.config/neofetch/
-            cp neofetch.conf ~/.config/neofetch/config.conf
+            echo "${GREEN}Installing neofetch config..."
+            #mkdir -pv ~/.config/neofetch/
+            #cp neofetch.conf ~/.config/neofetch/config.conf
             break;;
 
         [nN]* ) 
-            echo "Not installing neofetch config..."
+            echo "${YELLOW}Not installing neofetch config..."
             break;;
 
-        * ) echo "Invalid option entered."
+        * ) echo "${RED}Invalid option entered."
     esac
 done
 
-echo "Moving .zshrc..."
-cp .zshrc ~
-echo "Moving .p10k.zsh..."
-cp .p10k.zsh ~
+while true
+do
+    read -p "$(echo ${BG_RED}${BLACK}"Overwrite${NC}${BLUE} .zshrc and .p10k.zsh? [y/n]") " answer
+    case $answer in
+        [yY]* ) 
+            echo "${GREEN}Installing zsh and p10k configs..."
+            echo "${NC}Moving .zshrc..."
+            #cp .zshrc ~
+            echo "Moving .p10k.zsh..."
+            #cp .p10k.zsh ~
+            break;;
 
-echo "Moving completed!"
+        [nN]* ) 
+            echo "${YELLOW}Not overwriting zsh and p10k config..."
+            break;;
+
+        * ) echo "${RED}Invalid option entered."
+    esac
+done
+
+echo "${BG_GREEN}${BLACK}Operation completed!"
