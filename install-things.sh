89327ab52df7b56c4f4cd8af0a701372ffd0a66b
Will Gesler <willgesler@gmail.com>
2021-05-16 20:40:58 -0400

    these colors weren't working in bash

--- install-things.sh
+++ install-things.sh
@@ -1,65 +1,56 @@
-RED='\033[1;31m'
-BG_RED='\033[1;41m'
-BLUE='\033[1;34m'
-GREEN='\033[1;32m'
-BG_GREEN='\033[1;42m'
-BLACK='\033[1;30m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
 while true
 do
-    read -p "$(echo ${BLUE}"Install oh-my-zsh? [y/n]") " answer
+    read -p "$(echo "Install oh-my-zsh? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing oh-my-zsh...${NC}"
+            echo "Installing oh-my-zsh...${NC}"
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not installing oh-my-zsh..."
+            echo "Not installing oh-my-zsh..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
 
     esac
 done
 
 while true
 do
-    read -p "$(echo ${BLUE}"Install powerlevel10k? [y/n]") " answer
+    read -p "$(echo "Install powerlevel10k? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing powerlevel10k...${NC}"
+            echo "Installing powerlevel10k...${NC}"
             git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not installing powerlevel10k..."
+            echo "Not installing powerlevel10k..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
 
     esac
 done
 
 while true
 do
-    read -p "$(echo ${BLUE}"Install vimplug? [y/n]") " answer
+    read -p "$(echo "Install vimplug? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing vimplug...${NC}"
+            echo "Installing vimplug...${NC}"
             sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not installing vimplug..."
+            echo "Not installing vimplug..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
 
     esac
 done
 
-echo "${BG_GREEN}${BLACK}Operation completed!"
+echo "Operation completed!"
--- move-configs.sh
+++ move-configs.sh
@@ -1,67 +1,58 @@
-RED='\033[1;31m'
-BG_RED='\033[1;41m'
-BLUE='\033[1;34m'
-GREEN='\033[1;32m'
-BG_GREEN='\033[1;42m'
-BLACK='\033[1;30m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
-while true
+hile true
 do
-    read -p "$(echo ${BLUE}"Install init.vim for neovim? [y/n]") " answer
+    read -p "$(echo "Install init.vim for neovim? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing neovim config..."
+            echo "Installing neovim config..."
             mkdir -pv ~/.config/nvim/
             cp init.vim ~/.config/nvim/
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not installing neovim config..."
+            echo "Not installing neovim config..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
 
     esac
 done
 
 while true
 do
-    read -p "$(echo ${BLUE}"Install neofetch config? [y/n]") " answer
+    read -p "$(echo "Install neofetch config? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing neofetch config..."
+            echo "Installing neofetch config..."
             mkdir -pv ~/.config/neofetch/
             cp neofetch.conf ~/.config/neofetch/config.conf
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not installing neofetch config..."
+            echo "Not installing neofetch config..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
     esac
 done
 
 while true
 do
-    read -p "$(echo ${BG_RED}${BLACK}"Overwrite${NC}${BLUE} .zshrc and .p10k.zsh? [y/n]") " answer
+    read -p "$(echo "Overwrite .zshrc and .p10k.zsh? [y/n]") " answer
     case $answer in
         [yY]* ) 
-            echo "${GREEN}Installing zsh and p10k configs..."
-            echo "${NC}Moving .zshrc..."
+            echo "Installing zsh and p10k configs..."
+            echo "$Moving .zshrc..."
             cp .zshrc ~
             echo "Moving .p10k.zsh..."
             cp .p10k.zsh ~
             break;;
 
         [nN]* ) 
-            echo "${YELLOW}Not overwriting zsh and p10k config..."
+            echo "Not overwriting zsh and p10k config..."
             break;;
 
-        * ) echo "${RED}Invalid option entered."
+        * ) echo "Invalid option entered."
     esac
 done
 
-echo "${BG_GREEN}${BLACK}Operation completed!"
+echo "Operation completed!"
